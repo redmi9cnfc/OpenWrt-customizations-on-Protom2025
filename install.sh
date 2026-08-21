d95882fa49154e9e9b924c94818ff52986ae74b4
404unlocked <akulickirill71@gmail.com>
2026-08-21 12:44:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 RAW_URL="https://raw.githubusercontent.com/redmi9cnfc/OpenWrt-customizations-on-Protom2025/main"
 echo "Загрузка и установка файлов..."
-wget -qO /usr/sbin/router-banner "$RAW_URL/root/usr/sbin/router-banner"
-wget -qO /etc/init.d/router-banner "$RAW_URL/root/etc/init.d/router-banner"
 wget -qO /etc/config/router_banner "$RAW_URL/root/etc/config/router_banner"
 mkdir -p /usr/share/luci/menu.d
 mkdir -p /usr/share/rpcd/acl.d
@@ -13,12 +11,7 @@ wget -qO /usr/share/rpcd/acl.d/luci-app-router-banner.json "$RAW_URL/root/usr/sh
 wget -qO /www/luci-static/resources/view/router-banner.js "$RAW_URL/root/www/luci-static/resources/view/router-banner.js"
 wget -qO /www/luci-static/resources/router-banner/router-banner.css "$RAW_URL/root/www/luci-static/resources/router-banner/router-banner.css"
 wget -qO /www/luci-static/resources/router-banner/router-banner.js "$RAW_URL/root/www/luci-static/resources/router-banner/router-banner.js"
-echo "Настройка прав доступа..."
-chmod +x /usr/sbin/router-banner
-chmod +x /etc/init.d/router-banner
-echo "Запуск службы и обновление LuCI..."
-/etc/init.d/router-banner enable
-/etc/init.d/router-banner start
+echo "Обновление кэша LuCI..."
 rm -rf /tmp/luci-indexcache /tmp/luci-modulecache/
 /etc/init.d/rpcd restart
 /etc/init.d/uhttpd restart
