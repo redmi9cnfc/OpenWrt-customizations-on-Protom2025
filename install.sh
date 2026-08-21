70509bcf98531740a5be5f61d28f29126cfa94ab
404unlocked <akulickirill71@gmail.com>
2026-08-21 12:49:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 RAW_URL="https://raw.githubusercontent.com/redmi9cnfc/OpenWrt-customizations-on-Protom2025/main"
-echo "Загрузка и установка файлов..."
+echo "Downloading and installing files..."
 wget -qO /etc/config/router_banner "$RAW_URL/root/etc/config/router_banner"
 mkdir -p /usr/share/luci/menu.d
 mkdir -p /usr/share/rpcd/acl.d
@@ -11,8 +11,8 @@ wget -qO /usr/share/rpcd/acl.d/luci-app-router-banner.json "$RAW_URL/root/usr/sh
 wget -qO /www/luci-static/resources/view/router-banner.js "$RAW_URL/root/www/luci-static/resources/view/router-banner.js"
 wget -qO /www/luci-static/resources/router-banner/router-banner.css "$RAW_URL/root/www/luci-static/resources/router-banner/router-banner.css"
 wget -qO /www/luci-static/resources/router-banner/router-banner.js "$RAW_URL/root/www/luci-static/resources/router-banner/router-banner.js"
-echo "Обновление кэша LuCI..."
+echo "Updating LuCI cache..."
 rm -rf /tmp/luci-indexcache /tmp/luci-modulecache/
 /etc/init.d/rpcd restart
 /etc/init.d/uhttpd restart
-echo "Установка завершена! Перезагрузите страницу в браузере (Ctrl+F5)."
+echo "Installation complete! Refresh the page in your browser (Ctrl+F5)"
