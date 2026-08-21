b8beed119e526e0f6e1986fcfa4e79ad8c29ef58
404unlocked <akulickirill71@gmail.com>
2026-08-21 12:27:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,25 @@
 #!/bin/sh
-echo "Загрузка пакета..."
-wget -qO /tmp/router-banner.tar.gz https://github.com/redmi9cnfc/OpenWrt-customizations-on-Protom2025/raw/main/router-banner.tar.gz
-
-if [ ! -f /tmp/router-banner.tar.gz ]; then
-    echo "Ошибка: не удалось скачать файл!"
-    exit 1
-fi
-
-
-echo "Установка файлов..."
-tar -xzf /tmp/router-banner.tar.gz -C /
-
-
-rm -f /tmp/router-banner.tar.gz
-
-
+RAW_URL="https://raw.githubusercontent.com/redmi9cnfc/OpenWrt-customizations-on-Protom2025/main"
+echo "Загрузка и установка файлов..."
+wget -qO /usr/sbin/router-banner "$RAW_URL/root/usr/sbin/router-banner"
+wget -qO /etc/init.d/router-banner "$RAW_URL/root/etc/init.d/router-banner"
+wget -qO /etc/config/router_banner "$RAW_URL/root/etc/config/router_banner"
+mkdir -p /usr/share/luci/menu.d
+mkdir -p /usr/share/rpcd/acl.d
+mkdir -p /www/luci-static/resources/view
+mkdir -p /www/luci-static/resources/router-banner
+wget -qO /usr/share/luci/menu.d/luci-app-router-banner.json "$RAW_URL/root/usr/share/luci/menu.d/luci-app-router-banner.json"
+wget -qO /usr/share/rpcd/acl.d/luci-app-router-banner.json "$RAW_URL/root/usr/share/rpcd/acl.d/luci-app-router-banner.json"
+wget -qO /www/luci-static/resources/view/router-banner.js "$RAW_URL/root/www/luci-static/resources/view/router-banner.js"
+wget -qO /www/luci-static/resources/router-banner/router-banner.css "$RAW_URL/root/www/luci-static/resources/router-banner/router-banner.css"
+wget -qO /www/luci-static/resources/router-banner/router-banner.js "$RAW_URL/root/www/luci-static/resources/router-banner/router-banner.js"
 echo "Настройка прав доступа..."
-chmod +x /usr/sbin/router-banner 2>/dev/null[cite: 1]
-chmod +x /etc/init.d/router-banner 2>/dev/null[cite: 1]
-
-
-echo "Запуск службы..."
-/etc/init.d/router-banner enable 2>/dev/null[cite: 1]
-/etc/init.d/router-banner start 2>/dev/null[cite: 1]
-
-
-echo "Обновление LuCI..."
+chmod +x /usr/sbin/router-banner
+chmod +x /etc/init.d/router-banner
+echo "Запуск службы и обновление LuCI..."
+/etc/init.d/router-banner enable
+/etc/init.d/router-banner start
 rm -rf /tmp/luci-indexcache /tmp/luci-modulecache/
 /etc/init.d/rpcd restart
 /etc/init.d/uhttpd restart
-
-echo "Установка успешно завершена! Обновите страницу в браузере (Ctrl+F5)."
+echo "Установка завершена! Перезагрузите страницу в браузере (Ctrl+F5)."
